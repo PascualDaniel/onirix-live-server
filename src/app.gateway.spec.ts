e79ef383f6fdd0f0dcc0d339d019fd2c2e9bc2c3
Daniel Pascual López <uo269728@uniovi.es>
2024-01-05 18:31:48 +0100

    cambio guardados

--- src/app.gateway.spec.ts
+++ src/app.gateway.spec.ts
@@ -20,6 +20,11 @@ describe('AppGateway', () => {
     appGateway.server = server;
   });
 
+  afterEach(() => {
+ 
+    client.close();
+  });
+
 it('should handle broadcast', () => {
     const dto: PublishDTO = { 
             sender: 'sender',
@@ -31,5 +36,22 @@ it('should handle broadcast', () => {
     expect(spy).toHaveBeenCalledWith(dto, client);
 });
 
+it('should handle send to room', () => {
+    const dto: PublishDTO = { 
+            sender: 'sender',
+            message: 'message',
+            room: 'room'
+     };
+    const spy = jest.spyOn(appGateway, 'handleSendToRoom');
+    appGateway.handleSendToRoom(dto, client as unknown as Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>);
+    expect(spy).toHaveBeenCalledWith(dto, client);
+});
+
+
+it('should handle join room', () => {
+    const spy = jest.spyOn(appGateway, 'handleJoinRoom');
+    appGateway.handleJoinRoom('room', client as unknown as Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>);
+    expect(spy).toHaveBeenCalledWith('room', client);
+});
 
 });
